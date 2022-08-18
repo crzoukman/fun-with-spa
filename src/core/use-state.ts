9ac2374363cfb1ada55bf80ad13ec610bb066a64
crzoukman <zoukman@yandex.ru>
2022-08-18 18:06:52 +0300

    refactor: useState

--- src/core/use-state.ts
+++ src/core/use-state.ts
@@ -3,19 +3,14 @@ import { init } from '..';
 const store = [] as any;
 let idx = 0;
 
-export function useState<T>(
-  initial: T,
-  renderAllowed: boolean = true,
-) {
+export function useState<T>(initial: T) {
   const state = store[idx] || initial;
   const _idx = idx;
 
   function setState(value: T) {
     store[_idx] = value;
 
-    if (renderAllowed) {
-      init();
-    }
+    init();
   }
 
   idx++;
